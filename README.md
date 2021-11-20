e96b23fb253d69c895477cf563d40de2c1227c2e
maudin <maudin@bearstech.com>
2021-11-20 19:04:36 +0100

    working calls

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # alerta-generic-plugin
-Alerta
+Alerta generic plugin, run /etc/alerta/{pre_receive,post_receive,status_change} scripts if they exists with str(alert) as argument
--- alerta_generic_plugin.py
+++ alerta_generic_plugin.py
@@ -1,13 +1,5 @@
-import logging
 from alerta.plugins import PluginBase
 import os.path
-
-logging.basicConfig(filename='/var/log/alerta/alerta.log',
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='-%Y-%m-%d %H:%M:%S',
-                    level=logging.INFO)
-LOG = logging.getLogger('alerta.plugins.alertagenericplugin')
 
 class AlertaGenericPlugin(PluginBase):
     def pre_receive(self, alert):
@@ -16,13 +8,11 @@ class AlertaGenericPlugin(PluginBase):
         return alert
 
     def post_receive(self, alert):
-        LOG.debug(alert)
         if os.path.isfile('/etc/alerta/post_receive'):
             os.system('/etc/alerta/post_receive "%s"' % alert)
         return
 
     def status_change(self, alert, status, text):
-        LOG.info(alert)
         if os.path.isfile('/etc/alerta/status_change'):
             os.system('/etc/alerta/status_change "%s"' % alert)
         return
