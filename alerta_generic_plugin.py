23cb058a68d11b8e1441c7b2d16b00f5280b5f5a
maudin <maudin@bearstech.com>
2021-11-20 18:08:21 +0100

    better logs, use global LOG

--- alerta_generic_plugin.py
+++ alerta_generic_plugin.py
@@ -2,7 +2,11 @@ import logging
 from alerta.plugins import PluginBase
 import os.path
 
-logging.basicConfig(filename='/var/log/alerta/alerta.log', level=logging.INFO)
+logging.basicConfig(filename='/var/log/alerta/alerta.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='-%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
 LOG = logging.getLogger('alerta.plugins.alertagenericplugin')
 
 class AlertaGenericPlugin(PluginBase):
@@ -12,13 +16,13 @@ class AlertaGenericPlugin(PluginBase):
         return alert
 
     def post_receive(self, alert):
-        logging.debug(alert)
+        LOG.debug(alert)
         if os.path.isfile('/etc/alerta/post_receive'):
             os.system('/etc/alerta/post_receive "%s"' % alert)
         return
 
     def status_change(self, alert, status, text):
-        logging.info(alert)
+        LOG.info(alert)
         if os.path.isfile('/etc/alerta/status_change'):
             os.system('/etc/alerta/status_change "%s"' % alert)
         return
