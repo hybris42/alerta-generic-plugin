4f3c6005266b8983c10a336fda57d093f73368a2
maudin <maudin@bearstech.com>
2021-11-20 17:52:30 +0100

    log in /var/log/alerta/alerta.log, execute pre/post/change scripts if they exists

--- alerta_generic_plugin.py
+++ alerta_generic_plugin.py
@@ -1,15 +1,24 @@
 import logging
 from alerta.plugins import PluginBase
+import os.path
 
+logging.basicConfig(filename='/var/log/alerta/alerta.log', level=logging.INFO)
 LOG = logging.getLogger('alerta.plugins.alertagenericplugin')
 
 class AlertaGenericPlugin(PluginBase):
     def pre_receive(self, alert):
+        if os.path.isfile('/etc/alerta/pre_receive'):
+            os.system('/etc/alerta/pre_receive "%s"' % alert)
         return alert
 
     def post_receive(self, alert):
-        print(alert)
+        logging.debug(alert)
+        if os.path.isfile('/etc/alerta/post_receive'):
+            os.system('/etc/alerta/post_receive "%s"' % alert)
         return
 
     def status_change(self, alert, status, text):
+        logging.info(alert)
+        if os.path.isfile('/etc/alerta/status_change'):
+            os.system('/etc/alerta/status_change "%s"' % alert)
         return
