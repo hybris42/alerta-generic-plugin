2cabcde7bcb26d1e4a58388bbe09e45c561e40fe
maudin <maudin@bearstech.com>
2021-11-21 11:36:05 +0100

    handle status_change in post_receive (alert object is alerte before status change...)

--- alerta_generic_plugin.py
+++ alerta_generic_plugin.py
@@ -2,17 +2,35 @@ from alerta.plugins import PluginBase
 import os.path
 
 class AlertaGenericPlugin(PluginBase):
+    def __init__(self, name=None):
+        self.changed = []
+        super(AlertaGenericPlugin, self).__init__(name)
+
     def pre_receive(self, alert):
         if os.path.isfile('/etc/alerta/pre_receive'):
-            os.system('/etc/alerta/pre_receive "%s"' % alert)
+            os.system('/etc/alerta/pre_receive %s %s %s %s %s' % (alert.id,
+                                                                  alert.environment,
+                                                                  alert.resource,
+                                                                  alert.event,
+                                                                  alert.severity))
         return alert
 
     def post_receive(self, alert):
+        if alert.id in self.changed and os.path.isfile('/etc/alerta/status_change'):
+            os.system('/etc/alerta/status_change %s %s %s %s %s' % (alert.id,
+                                                                    alert.environment,
+                                                                    alert.resource,
+                                                                    alert.event,
+                                                                    alert.severity))
+            self.changed.remove(alert.id)
         if os.path.isfile('/etc/alerta/post_receive'):
-            os.system('/etc/alerta/post_receive "%s"' % alert)
+            os.system('/etc/alerta/post_receive %s %s %s %s %s' % (alert.id,
+                                                                   alert.environment,
+                                                                   alert.resource,
+                                                                   alert.event,
+                                                                   alert.severity))
         return
 
     def status_change(self, alert, status, text):
-        if os.path.isfile('/etc/alerta/status_change'):
-            os.system('/etc/alerta/status_change "%s"' % alert)
+        self.changed.append(alert.id)
         return
